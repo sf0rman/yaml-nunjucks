29efffee551ea88b697e95448baca557ae5829ee
Sebastian Forman <sebastian.forman@silverflow.com>
2026-02-13 17:56:10 +0100

    Update highlighting to match Go-lang color scheme

--- languages/yaml-nunjucks/highlights.scm
+++ languages/yaml-nunjucks/highlights.scm
@@ -1,37 +1,33 @@
 ; YAML + Nunjucks + CloudFormation highlighting
+; Color scheme matches Go-lang style
 
 ; === Nunjucks ===
-; Keywords (if, for, set, etc.)
+; Keywords (if, for, set, etc.) - blue like Go keywords
 (nunjucks_keyword) @keyword.control
 
-; Statement content
-(nunjucks_statement) @tag
-
-; Expression content
+; Variables/identifiers in expressions {{ }} - white/light like Go identifiers
 (nunjucks_expression) @variable
 
-; Comments
+; Comments - gray
 (nunjucks_comment) @comment
 
 ; === CloudFormation ===
-; Intrinsic function tags (!Ref, !Sub, etc.)
+; Intrinsic function tags (!Ref, !Sub, etc.) - function style
 (cf_tag) @function.builtin
-
-; Complete intrinsic function calls
 (cf_intrinsic (cf_tag) @function.builtin)
 
 ; === YAML Structure ===
-; Keys in key-value pairs
-(yaml_pair (yaml_key) @property)
+; Keys - neutral/light (like Go struct fields)
+(yaml_pair (yaml_key) @variable.member)
 
-; Quoted strings
+; Quoted strings - orange/brown like Go strings
 (yaml_string) @string
 
-; Plain scalar values
-(yaml_value) @string.special
+; Plain scalar values - keep as strings for consistency
+(yaml_value) @string
 
 ; Mixed content (text + templates)
-(yaml_mixed) @string.special
+(yaml_mixed) @string
 
-; YAML comments
-(comment) @comment.line
+; YAML comments - gray
+(comment) @comment
