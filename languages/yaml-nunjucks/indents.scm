182b74d7f3282918e67a28aefed4c2a5da4ba9e8
Sebastian Forman <sebastian.forman@silverflow.com>
2026-02-13 17:47:04 +0100

    Remove invalid query files - only keep highlights.scm with valid node types

--- languages/yaml-nunjucks/indents.scm
+++
@@ -1,3 +0,0 @@
-; Minimal indent rule required by Zed
-; Jinja2 expressions don't need special indentation
-(jinja_expression) @indent
--- languages/yaml-nunjucks/injections.scm
+++
@@ -1,1 +0,0 @@
-; No injections needed - using combined grammar
