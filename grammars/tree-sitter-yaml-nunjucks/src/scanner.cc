faf135a70d10f215a153ef4cf0f0dfd18c611d03
Sebastian Forman <sebastian.forman@silverflow.com>
2026-05-29 18:04:50 +0200

    Fix external scanner symbol names to match yaml_nunjucks grammar name

--- grammars/tree-sitter-yaml-nunjucks/src/scanner.cc
+++ grammars/tree-sitter-yaml-nunjucks/src/scanner.cc
@@ -954,26 +954,26 @@ struct Scanner {
 
 extern "C" {
 
-void *tree_sitter_yaml_external_scanner_create() {
+void *tree_sitter_yaml_nunjucks_external_scanner_create() {
   return new Scanner();
 }
 
-void tree_sitter_yaml_external_scanner_destroy(void *payload) {
+void tree_sitter_yaml_nunjucks_external_scanner_destroy(void *payload) {
   Scanner *scanner = static_cast<Scanner *>(payload);
   delete scanner;
 }
 
-unsigned tree_sitter_yaml_external_scanner_serialize(void *payload, char *buffer) {
+unsigned tree_sitter_yaml_nunjucks_external_scanner_serialize(void *payload, char *buffer) {
   Scanner *scanner = static_cast<Scanner *>(payload);
   return scanner->serialize(buffer);
 }
 
-void tree_sitter_yaml_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
+void tree_sitter_yaml_nunjucks_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
   Scanner *scanner = static_cast<Scanner *>(payload);
   scanner->deserialize(buffer, length);
 }
 
-bool tree_sitter_yaml_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
+bool tree_sitter_yaml_nunjucks_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
   Scanner *scanner = static_cast<Scanner *>(payload);
   return scanner->scan(lexer, valid_symbols);
 }
